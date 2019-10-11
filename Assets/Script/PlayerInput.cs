87b11636bb67362b3f29896daebf73ef7b5a2d7a
bruno takashi <bt.tengan@gmail.com>
2019-10-11 13:36:33 -0300

    Início básico, personagem se movendo.
    - movimentação limitada em 4 direções.
    - o personagem se rotaciona para a direção que irá andar.

---
+++ Assets/Script/PlayerInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	float _horizontal, _vertical;
+	bool _horizontalDown, _verticalDown;
+
+	void Update() {
+		// A (-1) <-> D (1).
+		_horizontal = Input.GetAxis("Horizontal");
+		// S (-1) <-> W (1).
+		_vertical = Input.GetAxis("Vertical");
+
+		if (Input.GetButtonDown("Horizontal")) {
+			_horizontalDown = true;
+		}
+		if (Input.GetButtonDown("Vertical")) {
+			_verticalDown = true;
+		}
+	}
+
+	public float GetHorizontal() {
+		return _horizontal;
+	}
+
+	public bool IsHorizontalDown() {
+		if (_horizontalDown) {
+			_horizontalDown = false;
+			return true;
+		}
+		return false;
+	}
+
+	public float GetVertical() {
+		return _vertical;
+	}
+
+	public bool IsVerticalDown() {
+		if (_verticalDown) {
+			_verticalDown = false;
+			return true;
+		}
+		return false;
+	}
+}
---
+++ Assets/Script/Walk.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+	PlayerInput _pi;
+	Rigidbody _rb;
+
+	Vector3 _newOrientation, _oldOrientation, _orientation;
+	[SerializeField] float _timeRotationLerp = 0.00001f;
+	float _deltaTime;
+
+	[SerializeField] float _velocity = 0;
+
+	void Start() {
+		_pi = gameObject.GetComponent<PlayerInput>();
+		_rb = gameObject.GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		// velocity.
+		if (_pi.GetHorizontal() != 0) {
+			_rb.velocity = Vector3.right * _pi.GetHorizontal() * _velocity;
+		}
+		else if (_pi.GetVertical() != 0) {
+			_rb.velocity = Vector3.forward * _pi.GetVertical() * _velocity;
+		}
+		else {
+			_rb.velocity = Vector3.zero;
+		}
+
+		// orientation.
+		if (_pi.IsHorizontalDown()) {
+			if (_pi.GetHorizontal() > 0) {
+				_newOrientation = Vector3.up * 90;
+				_oldOrientation = transform.rotation.eulerAngles;
+			}
+			else {
+				if (transform.rotation.eulerAngles.y < 90) {
+					_oldOrientation = Vector3.up * (transform.rotation.eulerAngles.y + 360);
+				}
+				else {
+					_oldOrientation = transform.rotation.eulerAngles;
+				}
+				_newOrientation = Vector3.up * 270;
+			}
+			_deltaTime = 0;
+		}
+		if (_pi.IsVerticalDown()) {
+			if (_pi.GetVertical() > 0) {
+				if (transform.rotation.eulerAngles.y > 180) {
+					_newOrientation = Vector3.up * 360;
+				}
+				else {
+					_newOrientation = Vector3.zero;
+				}
+				_oldOrientation = transform.rotation.eulerAngles;
+			}
+			else {
+				_newOrientation = Vector3.up * 180;
+				_oldOrientation = transform.rotation.eulerAngles;
+			}
+			_deltaTime = 0;
+		}
+
+		_deltaTime += Time.fixedDeltaTime;
+		_orientation = Vector3.Lerp(_oldOrientation, _newOrientation, _deltaTime / _timeRotationLerp);
+		_rb.MoveRotation(Quaternion.Euler(_orientation));
+	}
+}
