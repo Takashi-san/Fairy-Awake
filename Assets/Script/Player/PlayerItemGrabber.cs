04aaea7a9887097100948299ec63013d0842c667
Igor Beduin <igor.rob.96@gmail.com>
2019-10-31 10:42:31 -0300

    grab tests FIXED

---
+++ Assets/Script/Player/PlayerItemGrabber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemGrabber : MonoBehaviour
+{   
+    PlayerInput _pi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pi = GetComponent<PlayerInput>();
+        if (!_pi) 
+        {
+            Debug.LogError("No PlayerInput in the object!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    void OnCollisionEnter(Collision other) {
+        Debug.Log(other.gameObject.name);
+    }
+}
+
