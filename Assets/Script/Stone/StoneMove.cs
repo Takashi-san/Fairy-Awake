bc3f47dcab53e40935d76cdff137c2b27ee1b542
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2019-11-11 21:42:55 -0300

    Rolling Stone
    Stone move when player interact

--- Assets/Script/Stone/StoneMove.cs
+++ Assets/Script/Stone/StoneMove.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StoneMove : MonoBehaviour
 {
     public enum State{
@@ -17,7 +18,15 @@ public class StoneMove : MonoBehaviour
     Rigidbody _rb;
     public GameObject Player;
 
+    public static class Const {
+        public const float TimeToReset = 2f;
+    }
+
     public float radius = 1.7f;
+    public float timer = Const.TimeToReset;
+    [SerializeField] int _velToMove = 2;
+    float _dist_x;
+    float _dist_z;
 
     void OnDrawGizmosSelected () {
         Gizmos.color = Color.yellow;
@@ -43,12 +52,42 @@ public class StoneMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_pi.GetInteract() && Vector3.Distance(Player.transform.position, transform.position) <= radius){
-            state = StoneMove.State.move;
-            Debug.Log("Close enough");
-        }
-        else{
-            state = StoneMove.State.stop;
+        switch (state)
+        {
+            case StoneMove.State.stop:
+                if (_pi.GetInteract() && Vector3.Distance(Player.transform.position, transform.position) <= radius){
+                    state = StoneMove.State.move;
+                    Debug.Log("Close enough");
+                    Debug.Log("State = move");
+                    _dist_x= transform.position.x - Player.transform.position.x;
+                    _dist_z= transform.position.z - Player.transform.position.z;
+                    Debug.Log("X: " + _dist_x + ", Z: " + _dist_z);
+                }
+                _rb.useGravity = true;
+                break;
+            case StoneMove.State.inbetween:
+                
+                timer -= Time.deltaTime;
+                if (timer <= 0){
+                    timer = Const.TimeToReset;
+                    state = StoneMove.State.stop;
+                    _rb.velocity = Vector3.zero;
+                    _rb.useGravity = true;
+                }
+                break;
+            case StoneMove.State.move:
+                _rb.useGravity = false;
+                if( Mathf.Abs(_dist_z) <  Mathf.Abs(_dist_x)){
+                    _rb.velocity = new Vector3(_velToMove * _dist_x / Mathf.Abs(_dist_x), 0, 0);
+                }
+                else {
+                    _rb.velocity = new Vector3(0, 0, _velToMove * _dist_z / Mathf.Abs(_dist_z));
+                }
+                state = StoneMove.State.inbetween;
+                break;
+            default:
+                state = StoneMove.State.stop;
+                break;
         }
     }
 
