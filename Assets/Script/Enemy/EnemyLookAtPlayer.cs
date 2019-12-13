5dc1c722d04f81febaccf741eace91be85a37d61
bruno takashi <bt.tengan@gmail.com>
2019-12-13 11:28:23 -0300

    Ajustes transição de fases

--- Assets/Script/Enemy/EnemyLookAtPlayer.cs
+++ Assets/Script/Enemy/EnemyLookAtPlayer.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class EnemyLookAtPlayer : MonoBehaviour {
 	Transform _player;
+	Rigidbody _rb;
+	[SerializeField] float _velocity = 0;
 
 	void Start() {
+		_rb = GetComponent<Rigidbody>();
 		_player = FindObjectOfType<PlayerMovement>().transform;
 		if (!_player) {
 			Debug.LogError("Player not found to follow!");
@@ -14,5 +17,9 @@ public class EnemyLookAtPlayer : MonoBehaviour {
 
 	void Update() {
 		transform.LookAt(new Vector3(_player.transform.position.x, transform.position.y, _player.transform.position.z));
+	}
+
+	void FixedUpdate() {
+		_rb.velocity = transform.rotation.eulerAngles.normalized * _velocity;
 	}
 }
