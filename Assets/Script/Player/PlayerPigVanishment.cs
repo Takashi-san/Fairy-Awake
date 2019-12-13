4e6b778089d69780079cb289ef7aa51fe1b8b1f7
bruno takashi <bt.tengan@gmail.com>
2019-12-12 21:04:08 -0300

    Ajuste no prefab MC com a flauta

--- Assets/Script/Player/PlayerPigVanishment.cs
+++ Assets/Script/Player/PlayerPigVanishment.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPigVanishment : MonoBehaviour
-{
-    PlayerGrabSystem _grabSystem;
-    [SerializeField] GameObject _pig;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _grabSystem = FindObjectOfType<PlayerGrabSystem>().GetComponent<PlayerGrabSystem>();
-    }
+public class PlayerPigVanishment : MonoBehaviour {
+	[SerializeField] PlayerGrabSystem _grabSystem;
+	[SerializeField] GameObject _pig;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_grabSystem.IsGrabbing()) {
-            if (!_pig.activeSelf) {
-                _pig.SetActive(true);
-            }
-        } else if (_pig.activeSelf) {
-            _pig.SetActive(false);
-        }
-    }
+	void Start() {
+		//_grabSystem = gameObject.GetComponent<PlayerGrabSystem>();
+		if (!_grabSystem) {
+			Debug.Log("no grabsystem");
+		}
+	}
+
+	void Update() {
+		if (_grabSystem.IsGrabbing()) {
+			if (!_pig.activeSelf) {
+				_pig.SetActive(true);
+			}
+		}
+		else if (_pig.activeSelf) {
+			_pig.SetActive(false);
+		}
+	}
 }
