7f2a1cfeb00fdcc8a30818891565fe30146f1e21
Igor Beduin <igor.rob.96@gmail.com>
2019-12-12 23:28:38 -0300

    Porco que levanta itens aumentado (e arrumado a refer a null objects)

--- Assets/Script/Player/PlayerPigVanishment.cs
+++ Assets/Script/Player/PlayerPigVanishment.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class PlayerPigVanishment : MonoBehaviour {
 	[SerializeField] PlayerGrabSystem _grabSystem;
 	[SerializeField] GameObject _pig;
-
-	void Start() {
-		//_grabSystem = gameObject.GetComponent<PlayerGrabSystem>();
-		if (!_grabSystem) {
-			Debug.Log("no grabsystem");
-		}
-	}
+	[SerializeField] GameObject _flute;
 
 	void Update() {
-		if (_grabSystem.IsGrabbing()) {
-			if (!_pig.activeSelf) {
-				_pig.SetActive(true);
+		if (_flute != null) {
+			if (_flute.activeSelf) {
+				_grabSystem = _flute.GetComponent<PlayerGrabSystem>();
+				if (_grabSystem.IsGrabbing()) {
+					if (!_pig.activeSelf) {
+						_pig.SetActive(true);
+					}
+				}
+				else if (_pig.activeSelf) {
+					_pig.SetActive(false);
+				}
+			} else {
+				_flute = null;
+				_grabSystem = null;
 			}
-		}
-		else if (_pig.activeSelf) {
-			_pig.SetActive(false);
 		}
 	}
 }
